11dcf6190c8389345eed3b471a98015ec6034687
Mike78-87 <pudrovmike@gmail.com>
2025-05-28 12:51:35 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,8 +15,14 @@ def register_admin():
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
 
